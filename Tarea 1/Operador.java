2e6a4fa30b3450a75206429e8f6985d13e301900
PedroCisArce <102102331+PedroCisArce@users.noreply.github.com>
2022-05-01 21:14:19 -0400

    Update Operador.java

--- Tarea 1/Operador.java
+++ Tarea 1/Operador.java
@@ -20,22 +20,6 @@ public class Operador {
         }
     }
 
-    public double aprox(double n){ //uso de este método por evitar errores en el output del csv para el tiempo
-        if(n>=10){
-            double div = n/2;
-            String num = String.format("%.1f",div);
-            String numstring = num.charAt(0)+"."+num.charAt(2);
-            double res = Double.parseDouble(numstring)*2;
-            return res;
-        }
-        else{
-            String num = String.format("%.1f",n);
-            String numstring = num.charAt(0)+"."+num.charAt(2);
-            double res = Double.parseDouble(numstring);
-            return res;
-        }
-    }
-
     public void ExecuteCommands(){
         cloudCortina cortinas = new cloudCortina(data.get(0).get(0)); // guardamos el cloud de cortinas
         cloudLamp lamps = new cloudLamp(data.get(0).get(1));          //guardamos el cloud de lamparas
@@ -60,7 +44,7 @@ public class Operador {
             int commandTime = Integer.parseInt(data.get(it+1).get(0));
             System.out.println("DATOS: "+data.get(it)); //COMPROBACION DE DATOS
             Action accion = new Action(data.get(it));
-            while(aprox(time)<=commandTime){                                  //este ciclo nos sirve para entregar los estados cada 0.1 seg. de cada objeto  
+            while(time<=commandTime){                                  //este ciclo nos sirve para entregar los estados cada 0.1 seg. de cada objeto  
                 if(accion.getClase().equals("L")){       //vemos si la accion corresponde a L de lamparas para accionarla.
                     for(LampControl lc : ctrleslamp){
                         if(lc.getChannel() == accion.getCanal()){
@@ -83,10 +67,10 @@ public class Operador {
                     }
                 }
                 System.out.print(String.format("%.1f",time)+"\t"+cortinas.getStatus()+lamps.getStatus()+"\n");
-                time = aprox(time);
                 time+=delta;
             }
         }
+        System.out.print(String.format("%.1f",time)+"\t"+cortinas.getStatus()+lamps.getStatus()+"\n");
     }
     //atributos
     private List<List<String>> data;
